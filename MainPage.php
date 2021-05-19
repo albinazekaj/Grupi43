1bc262075000ed09b3ac8f5e1acba1a526eeb6c9
albinazekaj <58571492+albinazekaj@users.noreply.github.com>
2021-05-19 22:23:51 +0200

    Add files via upload

---
+++ MainPage.php
@@ -0,0 +1,113 @@
+			<?php include('header.php');
+			?>
+
+			<link rel="stylesheet" type="text/css" href="css/mainPage.css">
+			<link rel="stylesheet" href="./css/style.css">
+
+			<main>
+
+			    <div class="container">
+
+			        <div class="main-top">
+			            <nav>
+			                <ul>
+			                    <li  class="active">
+			                        <a href="MainPage.php">
+			                            HOME
+			                        </a>
+			                    </li>
+
+			                    <li>
+			                        <a href="Depertamentet.php">DEPERTAMENTET</a>
+			                    </li>
+			                    <li>
+			                        <a href="Rrethnesh.php">RRETH NESH</a>
+			                    </li>
+			                    
+			                    <li>
+                        			<a href="Stafi.php">STAFI</a>
+                   				 </li>
+
+			                </ul>
+
+			            </nav>
+			        </div>
+			        <!-- ==================================2========================================= -->
+
+			        <!-- **********************************3***************************************** -->
+			        <div class="maintop1">
+
+			            <div class="maintop2">
+			                <h1 id="logo" title="FIEK">FAKULTETI I INXHINIERISE ELEKTRIKE DHE KOMPJUTERIKE</h1>
+			                <p>
+			                    <u>FIEK STUDENTS</u>
+			                </p>
+			            </div>
+
+
+			        </div>
+			    </div>
+			    <section class="about-us-banner small-banner">
+			      <h4>Rruga drejt suksesit!</h4>
+			    </section>
+			    <!--***************************************4********************************************-->
+			    <section class="e1 our-commitment text-center grey-background">
+			        <div class="container">
+			            <h1 class="main-title">Lajme dhe ngjarje</h1>
+			            <div class="m1">
+			                <div class="w1 about-description">
+			                    <div class="m2">
+			                        <h3><b>Matematika 1 lista e studeteve ne salla </b></h3>
+			                        <p class="par">Listen e studenteve ne salla, per provimet me date 11.05.2021 nga Matematikes 1.</p>
+			                    </div>
+			                    <div class="m2">
+			                        <h3><b>Aplikimi online për vitin akademik 2022 për bursën KAEF është hapur!</b></h3>
+			                        <p class="par">Fondi Kosovaro-Amerikan për Edukim (KAEF) ofron 6 bursa të nivelit master në vit për Kosovarë të talentuar, që të studiojnë në universitetet më prestigjioze në SHBA.</p>
+			                    </div>
+			                </div>
+			                <div class="w2 about-description">
+			                    <div class="m2">
+			                        <h3><b>UP-ja dhe NewCo Ferronikeli nënshkruajnë marrëveshje</b></h3>
+			                        <p class="par">UP-ja dhe NewCo Ferronikeli nënshkruajnë marrëveshje për bashkëpunim në fushën profesionale dhe shkencore.</p>
+			                    </div>
+			                    <div class="m2">
+			                        <h3><b>Ligjëratë për teknologjinë 6G</b></h3>
+			                        <p class="par">Ligjëratë për teknologjinë 6G nga profesori Ian F. Akyildiz</p>
+			                    </div>
+			                </div>
+			            </div>
+			            <a href="Rrethnesh.php" class="btn-element">Read more about us</a>
+			        </div>
+			    </section>
+
+	<section id="boxes">
+		<table>
+			<tr id="box1">
+				<td class="cell1"><a href="https://fiek.uni-pr.edu/"><img src="./images/albina.png"></a></td>
+				<td class="cell2">
+					<h3>Albina Zekaj</h3>
+					<p><em>Une jam studentet e Fakultetit te Inxhinierise Elektrike dhe Kompjuterike,<br> dega Inxhinieri Kompjuterike.<br> Mendoj qe eshte nje mundesi shume e mire dhe me pelqen shum koha qe kaloi aty!</em></p></td>
+			</tr>
+			<tr id="box2">
+				<td class="cell3">
+					<h3>Ramiz Geci</h3>
+					<p><em>Une jam student ne degen e Telekomunikacion<br>Mesimi dhe pervoja qe ne perfitojme ne fakultet eshte shum e mire<br> ndjehem me fat qe jam nje student i ketij universiteti.<br>Click on the icon to see more!</em></p>
+				</td>
+				<td class="cell4"><a href="https://fiek.uni-pr.edu/page.aspx?id=1,14"><img src="./images/ramiz.png"></a></td>
+			</tr>
+			<tr id="box3">
+				<td class="cell5"><a href="https://www.youtube.com/watch?v=2BuNGAaQwEk"><img src="./images/altin.png"></a></td>
+				<td class="cell6">
+					<h3>Altin Sadriu</h3>
+					<p><em>Une jam student i deges se Elektronikes.<br>dhe une jam shum i knaqur nga mundesite qe neve studenteve na ofrohen ne fakultet</em></p>
+				</td>
+			</tr>
+		</table>
+	</section>
+			   
+			</main>
+			<script type="text/javascript" src="./javascript.js"></script>
+
+
+
+      <?php include('footer.php');?>
