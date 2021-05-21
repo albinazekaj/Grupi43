9e737fc20fa27132e983b6a58c8d6ed8c61c008c
albinazekaj <58571492+albinazekaj@users.noreply.github.com>
2021-05-21 20:54:34 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,78 @@
+<?php //include_once('serverSide.php') ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>FAKULTETI I INXHINIERISE ELEKTRIKE DHE KOMPJUTERIKE</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="js/jquery-1.11.2.min.js"></script>
+        <script type="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600" rel="stylesheet">
+        
+
+
+	</head>
+	<body id="Body">
+
+
+<header>
+    <div class="container">
+        <div class="header">
+            <div class="header-top">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="signup.php">Sign Up |</a>
+                        </li>
+                        <li>
+                            <a href="login.php">Login |</a>
+                        </li>
+                        
+                        <li>
+                        <a href="logout.php">Logout? |</a>
+                        </li>
+
+                        
+                    </ul>
+
+                </nav>
+            </div>
+            
+
+            <div class="header-bottom">
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com" class="linkedIn" target="_blank" title="LinkedIn">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/" class="twitter" target="_blank" title="Twitter">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.pinterest.com/" class="pinterest" target="_blank" title="Pinterest">
+                            <i class="fab fa-pinterest-p"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com" class="googleplus" target="_blank" title="Google Plus">
+                            <i class="fab fa-google-plus-g"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://rss.com" class="rss" target="_blank" title="Rss">
+                            <i class="fas fa-rss"></i>
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+
+</header>
