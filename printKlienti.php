f9edcd4ff65dcd8cd6cbc1a32e598312a67b8cdc
RamizGeci <83648013+RamizGeci@users.noreply.github.com>
2021-05-21 20:57:52 +0200

    Add files via upload

---
+++ printKlienti.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+	class printKlienti extends Klienti
+	{
+		
+		function __construct($emri,$mbiemri)
+		{
+			echo "Klienti: " . $emri . " " . $mbiemri . " eshte regjistruar!";
+		}
+	}
+
+
+ ?>  
